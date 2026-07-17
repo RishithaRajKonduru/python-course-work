5362360e07e5e0dd7d99c23598620d8b72d08f5a
K . Rishitha <krishitha2704@gmail.com>
2026-07-17 10:53:18 +0530

    Add files via upload

---
+++ day 22/date and time.py
@@ -0,0 +1,40 @@
+from datetime import date,time,datetime,timedelta
+
+'''t=date.today()
+print(t)
+print(t.year)
+print(t.month)
+print(t.day)
+print(t.weekday())
+print(t.isoweekday())'''
+
+'''year,month,day=list(map(int,input("Enter the dob[YYYY-MM-DD]:").split('-')))
+print(year,month,day)'''
+
+'''n=datetime.now()
+print(n.year)
+print(n.month)
+print(n.day)
+print(n.hour)
+print(n.minute)
+print(n.second)'''
+
+
+'''n=datetime.now()
+print(n.strftime('%Y/%m/%d'))
+print(n.strftime('%y/%m/%d'))
+print(n.strftime('%Y/%m/%d %H:%M:%S'))
+print(n.strftime('%y/%m/%d %I:%H:%M:%S %p'))
+print(n.strftime(' %a %y/%m/%d %%I:H:%M:%S %p'))
+print(n.strftime(' %A, %d %b %Y %I:%H:%M:%S %p'))
+print(n.strftime(' %A, %d %B %Y %I:%H:%M:%S %p'))'''
+
+'''n=datetime.now()
+d=date.today()
+a7=d-timedelta(days=20)
+a15=n+timedelta(minutes=15)
+
+print(a15)'''
+
+
+
