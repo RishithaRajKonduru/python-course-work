bf1fbb93104cd81baa7e678acb8c07abf791a4ad
K . Rishitha <krishitha2704@gmail.com>
2026-07-17 10:53:36 +0530

    Add files via upload

---
+++ day 23/oops.py
@@ -0,0 +1,43 @@
+'''class Flipkart:
+    products={'laptop':80000,
+              'phone':50000,
+              'earphones':900,
+              'bags':300}
+    @classmethod
+    def showProducts(cls):
+        print(cls.products)
+
+    def register(self,name,phno):
+        self.username = name
+        self.phonenumber = phno
+        print(f"Welcome to flipkart {self.username},shop now!!!")
+
+
+    @staticmethod
+    def discount():
+        print("Hey all,10% discount is going on ,show now!!!!!")
+
+saniya=Flipkart()
+saniya.register('saniya','987654321')
+saniya.discount()
+saniya.showProducts()
+
+
+
+
+rina=Flipkart()
+rina.register('rina','987654321')
+
+tina=Flipkart()
+tina.register('tina','987654321')'''
+
+
+'''class Flipkart:
+    def __init__(self,name,phoneno):
+        self.username = name
+        self.phonenumber = phoneno
+        print(f'Welcome to flipkart {self.username},shop now!!!')
+charan=Flipkart('charan','9876543210')'''
+
+
+
